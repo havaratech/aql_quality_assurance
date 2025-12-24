54cc94885ad7003b9b54836277093d33258a0b3c
rajguruhavaratech <g.raj@havaratech.com>
2025-12-24 12:14:32 +0000

    aql code updates

--- aql_quality_assurance/aql_quality_assurance_by_havaratech/doctype/quality_inspection/quality_inspection.py
+++ aql_quality_assurance/aql_quality_assurance_by_havaratech/doctype/quality_inspection/quality_inspection.py
@@ -21,5 +21,18 @@ def set_aql_parameters(doc, method):
     party_name = None
     party_type = None
     lot_size = 0
-    
-            
+
+    #3. Determine party details based on reference type.
+    if doc.reference_type in ["Purchase Receipt", "Purchase Invoice", "Subcontracting Receipt"]:
+        supplier_id =  ref_doc.supplier
+        if supplier_id:
+            #Fetch details directly from Supplier DocType
+            supplier_doc = frappe.get_cached_doc("Supplier", supplier_id)
+            party_name = supplier_doc.supplier_name
+            party_type = supplier_doc.supplier_type
+
+        lot_size = sum([item.qty for item in ref_doc.items if item.item_code == doc.item_code])    
+
+        doc.custom_aql_party_name = party_name
+        doc.custom_aql_party_type = party_type
+        doc.custom_aql_lot_size = lot_size
